3e531e7e529f1af6c1fff64e13c65b193fcb75bb
GamerDudeBYT <ethanbell15@outlook.com>
2024-08-06 21:32:00 +0100

    Add project files.

---
+++ BLE App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BLE_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BLE App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BLE_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
