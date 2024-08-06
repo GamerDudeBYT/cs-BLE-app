5c7ab51d99c6b0821dda7f96a7b3dbe7d60c33b2
GamerDudeBYT <ethanbell15@outlook.com>
2024-08-06 22:16:36 +0100

    testing

--- BLE App/MainPage.xaml.cs
+++ BLE App/MainPage.xaml.cs
@@ -11,7 +11,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            count+=10;
 
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
@@ -20,6 +20,12 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void Alert_Test(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "This is an alert", "I know that this is an alert");
+        }
     }
 
 }
+
